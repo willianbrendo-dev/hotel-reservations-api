190018c538c9cb34fceee9f7e8bd286a28396fa0
Willian Brendo <willianbrendo.dev@gmail.com>
2025-09-29 16:31:10 -0300

    Projeto Criado

---
+++ src/test/java/com/wb/hotel_reservations_api/HotelReservationsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wb.hotel_reservations_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
